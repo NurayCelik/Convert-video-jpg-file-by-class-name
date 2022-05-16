261e97ee37dc042cae601bafa7bb2433031cf52d
Nuray Çelik <viola.armoni@gmail.com>
2022-05-16 18:00:44 +0300

    Update separate_jpg_file_folder.py

--- separate_jpg_file_folder.py
+++ separate_jpg_file_folder.py
@@ -3,20 +3,24 @@ import random
 from pathlib import Path
 import shutil
 
-folder_paths = "/Convert-video-to-jpg-file-by-class-name/images/D61_1_3-2.mp4/"
+folder_paths = "images/D61_2_1-1.mp4/"
 
 count = 0
-directCount = 0
+directCount = 1
+    
 for f in os.listdir(folder_paths):
     Path("klasor__"+ str(directCount)+"/").mkdir(parents=True, exist_ok=True,mode=0o755)
-    des = "/Convert-video-to-jpg-file-by-class-name/klasor__"+ str(directCount)+"/"+str(f)
+    des = "/home/eventgates/Desktop/python/opencv/Convert-video-to-jpg-file-by-class-name/klasor__"+ str(directCount)+"/"+str(f)
     old_path = folder_paths+str(f)
+    file_count = len([name for name in os.listdir(folder_paths) if os.path.isfile(os.path.join(folder_paths, name))])
     if f.endswith('.jpg'):
             print(f)
-            if count %551 != 0: ### the number of files we want to separate
+            if count %559 != 0: ### the number of files we want to separate
               shutil.move(old_path, str(des))
             else:
               directCount +=1 
+              if file_count<=559:
+                  shutil.move(old_path, str(des))
               
     count +=1
     print(count)
